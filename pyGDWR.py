60ac24b290ea772d35447815950e9287cee61da3
PatNikVoe <patrick.niklas.voelkl@gmial.com>
2018-11-07 14:20:59 +0100

    adjusted the icon path so it will run from anywhere

--- pyGDWR.py
+++ pyGDWR.py
@@ -1,3 +1,4 @@
+import os, sys
 # import specific version of Notify to remove Warning
 import gi
 gi.require_version('Notify', '0.7')
@@ -11,7 +12,8 @@ Notify.init("GDWR")
 notification = Notify.Notification.new("Drink some water!")
 
 # Add icon
-image = GdkPixbuf.Pixbuf.new_from_file("icon.png")
+file = os.path.join(sys.path[0], 'icon.png')
+image = GdkPixbuf.Pixbuf.new_from_file(file)
 
 # Use the GdkPixbuf image
 notification.set_icon_from_pixbuf(image)
