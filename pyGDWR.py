8def13572e8de40e3c221999bc3234eb68c35248
PatNikVoe <patrick.niklas.voelkl@gmial.com>
2018-11-07 11:59:08 +0100

    Added comments; added import of specific version of Notify

--- pyGDWR.py
+++ pyGDWR.py
@@ -1,14 +1,21 @@
+# import specific version of Notify to remove Warning
+import gi
+gi.require_version('Notify', '0.7')
 # This time import the GdkPixbuf module
 from gi.repository import Notify, GdkPixbuf
 
+# Initialise
 Notify.init("GDWR")
+
+# Set Reminder Text here
 notification = Notify.Notification.new("Drink some water!")
 
-# Use GdkPixbuf to create the proper image type
+# Add icon
 image = GdkPixbuf.Pixbuf.new_from_file("icon.png")
 
 # Use the GdkPixbuf image
 notification.set_icon_from_pixbuf(image)
 notification.set_image_from_pixbuf(image)
 
+# Print Notification
 notification.show()
